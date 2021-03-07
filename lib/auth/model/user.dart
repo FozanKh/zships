6f3316d9ac11ea5d550f666b9b592ddd8b0c013b
Fozan Alkhalawi <FozanAlkhalawi@gmail.com>
2021-03-07 15:25:36 +0300

    update user model

--- lib/auth/model/user.dart
+++ lib/auth/model/user.dart
@@ -1,35 +1,23 @@
 class User {
   final String uid;
   final String name;
-  final String gender;
   final String email;
   final String phone;
   final bool isAnonymous;
   final bool isEmailVerified;
+  String apiKey;
 
-  User({this.uid, this.name, this.gender, this.email, this.phone, this.isAnonymous, this.isEmailVerified});
-
-  User copyWith({String uid, String name, String gender, String email, String phone, bool isAnonymous, bool isEmailVerified}) {
-    return User(
-      uid: uid ?? this.uid,
-      name: name ?? this.name,
-      gender: gender ?? this.gender,
-      email: email ?? this.email,
-      phone: phone ?? this.phone,
-      isAnonymous: isAnonymous ?? this.isAnonymous,
-      isEmailVerified: isEmailVerified ?? this.isEmailVerified,
-    );
-  }
+  User({this.uid, this.name, this.email, this.phone, this.isAnonymous, this.isEmailVerified, this.apiKey});
 
   Map<String, dynamic> toMap() {
     return {
       'uid': uid,
       'name': name,
-      'gender': gender,
       'email': email,
       'phone': phone,
       'isAnonymous': isAnonymous,
       'isEmailVerified': isEmailVerified,
+      'apiKey': apiKey,
     };
   }
 
@@ -39,16 +27,19 @@ class User {
     return User(
       uid: map['uid'],
       name: map['name'],
-      gender: map['gender'],
       email: map['email'],
       phone: map['phone'],
       isAnonymous: map['isAnonymous'],
       isEmailVerified: map['isEmailVerified'],
+      apiKey: map['apiKey'],
     );
+  }
+  setKey(String key) {
+    this.apiKey = key;
   }
 
   @override
   String toString() {
-    return 'User(uid: $uid, name: $name, gender: $gender, email: $email, phone: $phone, isAnonymous: $isAnonymous, isEmailVerified: $isEmailVerified)';
+    return 'User(uid: $uid, name: $name, email: $email, phone: $phone, isAnonymous: $isAnonymous, isEmailVerified: $isEmailVerified)';
   }
 }
