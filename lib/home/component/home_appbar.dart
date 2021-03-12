e0c7a6ca79ded35eceb4d4dca2d6dbbcbcba5bec
Fozan Alkhalawi <FozanAlkhalawi@gmail.com>
2021-03-12 08:54:56 +0300

    Update Custom Appbar

--- lib/home/component/home_appbar.dart
+++ lib/home/component/home_appbar.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zships/auth/model/user.dart';
 import 'package:zships/constants/colors.dart';
 import 'package:zships/localization/constants.dart';
 
-class HomeAppBar extends PreferredSize {
+class CustomAppBar extends PreferredSize {
   final double height;
+  final String title;
+  final bool trailingIcon;
+  final TextStyle textStyle;
+  final BuildContext context;
 
-  HomeAppBar({this.height = kToolbarHeight + 30});
+  User user;
+  CustomAppBar(this.context, {this.textStyle, this.title, this.trailingIcon = true, this.height = kToolbarHeight + 30});
 
   @override
   Size get preferredSize => Size.fromHeight(height);
 
   @override
   Widget build(BuildContext context) {
+    user = Provider.of(context);
     return AppBar(
       automaticallyImplyLeading: false,
       brightness: Brightness.dark,
@@ -19,25 +27,23 @@ class HomeAppBar extends PreferredSize {
       elevation: 0,
       toolbarHeight: kToolbarHeight + 30,
       actions: [
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 20, 20),
-          child: Image.asset('assets/images/shipment.png', color: kWhite, height: 50, width: 50),
-        ),
+        if (trailingIcon)
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 20, 20),
+            child: Image.asset('assets/images/shipment.png', color: kWhite, height: 50, width: 50),
+          ),
       ],
       flexibleSpace: Stack(
-        alignment: Alignment.topLeft,
         children: [
           Container(
+            alignment: trailingIcon ? Alignment.centerLeft : Alignment.center,
             height: kToolbarHeight + 30 + MediaQuery.of(context).padding.top,
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top, right: 20, left: 20, bottom: 15),
+            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top, right: 20, left: 20, bottom: 25),
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(color: kPrimaryColor, gradient: kMainGradient),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(getText(context, 'Welcome to zShips'), style: TextStyle(color: Colors.white, fontSize: 24)),
-              ],
+            child: Text(
+              title ?? getText(context, 'Welcome ${user.name ?? 'To zShips'}'),
+              style: textStyle ?? TextStyle(color: Colors.white, fontSize: 24),
             ),
           ),
           Align(
