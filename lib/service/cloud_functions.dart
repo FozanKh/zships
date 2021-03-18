fe03862561e41bd28119ec65df26347d6a80933f
Fozan Alkhalawi <FozanAlkhalawi@gmail.com>
2021-03-18 04:39:00 +0300

    Implement Cloud Functions

---
+++ lib/service/cloud_functions.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_functions/cloud_functions.dart';
+
+class CloudFunctions {
+  static CloudFunctions _instance;
+
+  static CloudFunctions get instance {
+    if (_instance == null) {
+      _instance = CloudFunctions();
+      return _instance;
+    } else
+      return _instance;
+  }
+
+  // getting the ApiKey through a cloud function to have it decrypted first
+  Future<String> getApiKey() async {
+    HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('getApiKey');
+    final results = await callable();
+    print(results.data);
+    return results.data;
+  }
+
+  // saving the ApiKey through a cloud function to have it encrypted first
+  Future<void> setApiKey(String apiKey) async {
+    HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('setApiKey');
+    await callable({"apiKey": apiKey});
+  }
+}
